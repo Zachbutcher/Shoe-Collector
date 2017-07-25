839c8e4f7aa578c88b1ff06516f68c1a01ea0acd
Zach Butcher <duke.fan04@yahoo.com>
2017-07-25 12:42:12 -0500

    add photo screen linked and layout completed

---
+++ Shoe Collector/CreateGameViewController.swift
@@ -0,0 +1,47 @@
+//
+//  CreateGameViewController.swift
+//  Shoe Collector
+//
+//  Created by Zach Butcher on 7/24/17.
+//  Copyright © 2017 Zach Butcher. All rights reserved.
+//
+
+import UIKit
+
+class CreateGameViewController: UIViewController {
+
+    @IBOutlet weak var imagePreview: UIImageView!
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func addPhoto(_ sender: Any) {
+        
+    }
+
+    @IBAction func takePhoto(_ sender: Any) {
+        
+    }
+    
+    @IBAction func addShoes(_ sender: UIButton) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
